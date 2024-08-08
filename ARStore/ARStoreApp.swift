73a6c9958af4dcd41e9e89d9458893157c858cdf
SMIT-2411 <smit24@icloud.com>
2024-08-08 16:58:54 +0530

    Initial Commit

---
+++ ARStore/ARStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARStoreApp.swift
+//  ARStore
+//
+//  Created by Smit Patel on 08/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ARStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
